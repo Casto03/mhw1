b909746c1a03f6a830936690fa300d87973adc2b
Casto03 <danycastorina46@gmail.com>
2025-05-18 09:49:01 +0200

    Update mhw3.js

--- mhw3.js
+++ mhw3.js
@@ -92,7 +92,7 @@ if (loginForm) {
 
         try {
             await auth0.loginWithRedirect({
-                redirect_uri: "http://127.0.0.1:5500/mhw1.html",
+                redirect_uri: "http://127.0.0.1:5500/mhw3.html",
                 authorizationParams: {
                     login_hint: email
                 }
